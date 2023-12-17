d4cf7bb1671156e2d1f8054fa4507adc2b4f5add
Leonardo Castro <141694339+Leonardocastr012@users.noreply.github.com>
2023-12-17 16:32:04 -0400

    I finished the hangman game and I used a lot of functions for the code to be more legible and I be able to change one part of that without to disturb the other part

--- forca.py
+++ forca.py
@@ -4,9 +4,9 @@ def titulo():
     print(titulo)
     print('*' * len(titulo))
 
-def gerando_palavra_secreta():# Puxando as palavras do arquivo txt
+def gerando_palavra_secreta(nome_arquivo='frutas.txt'):# Puxando as palavras do arquivo txte usei parâmetro opcional
     from random import randint
-    arquivo = open('frutas.txt', 'r', encoding='utf-8')
+    arquivo = open(nome_arquivo, 'r', encoding='utf-8')
     lista_frutas = []
     for linha in arquivo:
         linha_formatada = linha.strip('\n')
@@ -20,10 +20,101 @@ def gerando_palavra_secreta():# Puxando as palavras do arquivo txt
 def inicializa_letras_acertadas(palavra_secreta): #palavra_secreta é o parâmetro aqui
     return ['_' for letra in palavra_secreta]  # Vai ser adaptável
 
+
+def verificador_de_letra(chute, palavra_secreta, letras_acertadas):#tem que passar os parâmetros para funcionar
+    for pos, letra in enumerate(palavra_secreta):  # Vai pecorrer a palavra
+        if chute == letra:  # Vai verificar se tem a letra na palavra
+            letras_acertadas[pos] = chute  # Vai substuir na lista a letra na posição que o usuário acertar
+
+def desenha_forca(erros):
+    print("  _______     ")
+    print(" |/      |    ")
+
+    if(erros == 1):
+        print(" |      (_)   ")
+        print(" |            ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 2):
+        print(" |      (_)   ")
+        print(" |      \|    ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 3):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 4):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |            ")
+
+    if(erros == 5):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      /     ")
+
+    if (erros == 6):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      / \   ")
+
+    print(" |            ")
+    print("_|___         ")
+    print()
+
+
+def formacao_da_palavra_secreta(letras_acertadas):
+    for i in range(0, len(letras_acertadas)):  # Vai imprimir cada elemento da lista
+        print(f'{letras_acertadas[i]}', end='')
+    print()  # vai quebrar o end= para não grudar com a próxima linha
+
+
+def imprime_palavra_perdedor(palavra_secreta):
+    print('Puxa, você perdeu!')
+    print(f'A palavra era {palavra_secreta}')
+    print("    _______________         ")
+    print("   /               \       ")
+    print("  /                 \      ")
+    print("//                   \/\  ")
+    print("\|   XXXX     XXXX   | /   ")
+    print(" |   XXXX     XXXX   |/     ")
+    print(" |   XXX       XXX   |      ")
+    print(" |                   |      ")
+    print(" \__      XXX      __/     ")
+    print("   |\     XXX     /|       ")
+    print("   | |           | |        ")
+    print("   | I I I I I I I |        ")
+    print("   |  I I I I I I  |        ")
+    print("   \_             _/       ")
+    print("     \_         _/         ")
+    print("       \_______/           ")
+
+
+def imprime_mensagem_vencedor():
+    print("Parabéns, você ganhou!")
+    print("       ___________      ")
+    print("      '._==_==_=_.'     ")
+    print("      .-\\:      /-.    ")
+    print("     | (|:.     |) |    ")
+    print("      '-|:.     |-'     ")
+    print("        \\::.    /      ")
+    print("         '::. .'        ")
+    print("           ) (          ")
+    print("         _.' '._        ")
+    print("        '-------'       ")
+
 def jogo_forca():
 
     titulo()
-    palavra_secreta = gerando_palavra_secreta()
+    palavra_secreta = gerando_palavra_secreta('palavras.txt') #Aqui o parâmetro não é obrigatório é opcional
     letras_acertadas = inicializa_letras_acertadas(palavra_secreta)
 
     enforcou = False
@@ -31,29 +122,26 @@ def jogo_forca():
     erros = 0
     while not enforcou and not acertou: #True and True
         chute = str(input('Qual a letra? ')).strip().lower() #Vai tirar os espaços e vai ter as letras minúsculas
+        
         if chute in palavra_secreta: #Caso tenha a letra
             #Responsável por encontrar a letra e posição dela
-            for pos, letra in enumerate(palavra_secreta): #Vai pecorrer a palavra
-                if chute == letra: #Vai verificar se tem a letra na palavra
-                    letras_acertadas[pos] = chute #Vai substuir na lista a letra na posição que o usuário acertar
+            verificador_de_letra(chute, palavra_secreta, letras_acertadas)
         else:
             erros += 1
-            print(f'Ops, você errou! Faltam {len(palavra_secreta) - erros} tentativas!')
+            print(f'Ops, você errou! Faltam {6 - erros} tentativas!')
+        #desenhor da forca
+        desenha_forca(erros)
         # Responsável por printar na tela as letras ou underlines
-        for i in range(0, len(letras_acertadas)):  # Vai imprimir cada elemento da lista
-            print(f'{letras_acertadas[i]}', end='')
-        print()  # vai quebrar o end= para não grudar com a próxima linha
+        formacao_da_palavra_secreta(letras_acertadas)
+
         #Encerramento do jogo
         if erros == 6:
-            print('Você perdeu!')
+            imprime_palavra_perdedor(palavra_secreta)
             enforcou = True  # condição de parada
         elif '_' not in letras_acertadas: #Pois quando não tiver mais '_' vai ter completado a palavra
-            print('Você acertou!')
+            imprime_mensagem_vencedor()
             acertou = True
 
-    print('Fim de jogo!')
 
 if __name__ == '__main__':
     jogo_forca()
-
-#letras_faltando = str(letras_acertadas.count('_'))
